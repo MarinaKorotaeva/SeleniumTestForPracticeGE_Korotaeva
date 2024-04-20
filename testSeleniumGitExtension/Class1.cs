7f6e126df3753bdc3c464eb78c9b8dd5e32b3f66
MarinaKorotaeva <korotaevamg@mail.ru>
2024-04-20 13:02:29 +0500

    5 автотестов

--- testSeleniumGitExtension/Class1.cs
+++ testSeleniumGitExtension/Class1.cs
@@ -1,42 +1,121 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using FluentAssertions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace testSeleniumGitExtension;
 
 public class TestSeleniumForPractice
 {
+    public ChromeDriver driver;
+
+    [SetUp]
+    public void Setup()
+    {
+        Autorization();
+    }
+    
+    [Test] 
+    public void Autorizations()
+    {
+        var news = driver.FindElement(By.CssSelector("[data-tid='Title']"));
+        var currentUrl = driver.Url;
+        currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
+    }
+
     [Test]
+    public void NavigationTest()
+    {
+        var sideMenu = driver.FindElement(By.CssSelector("[data-tid='SidebarMenuButton']"));
+        sideMenu.Click();
+        
+        var communities = driver.FindElements(By.CssSelector("[data-tid='Community']"))
+            .First(element => element.Displayed);
+        communities.Click();
+
+        var communityTitle = driver.FindElements(By.CssSelector("[data-tid='Title']"));
+        Assert.That(driver.Url == "https://staff-testing.testkontur.ru/communities", message: "мы хотели, чтобы URL был https://staff-testing.testkontur.ru/communities");
+        
+    }
+
+    [Test]
+    public void DateOfBirth() //Этот тест и должен упасть: кнопка "сохранить" не кликабельная
+    {
+        var birthday = driver.FindElement(By.CssSelector("[data-tid='Title']"));
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/profile/settings/edit");
+        
+        var date = driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section[3]/div[1]/div[4]/label[2]/span"));
+        date.SendKeys("31.10.2000");
+        
+        var savechanges = driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section[1]/div[2]/button[1]"));
+        savechanges.Click();
+    }
+
+    [Test]
+    public void CreateFolder()
+    {
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(4));
+        wait.Until(ExpectedConditions.UrlContains("https://staff-testing.testkontur.ru/news"));
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/files");
+        
+        Assert.That(driver.Url == "https://staff-testing.testkontur.ru/files",
+            message: "мы хотели получить https://staff-testing.testkontur.ru/files, а получили " + driver.Url);
+
+        var add = driver.FindElement(By.XPath("/html/body/div/section/section[2]/div/div[2]/div[2]/div/div[2]/span/span/button"));
+        add.Click();
+
+        var createfolders = driver.FindElement(By.CssSelector("[data-tid='CreateFolder']"));
+        createfolders.Click();
+        
+        var folder = driver.FindElement(By.CssSelector("[data-tid='Input']"));
+        folder.SendKeys("NewFolder");
+        
+        var save = driver.FindElement(By.CssSelector("[data-tid='SaveButton']"));
+        //save.Click(); //закомментировала, чтобы не создавать сущности
+    }
+
+    [Test]
+    public void OnOffEmails()
+    {
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(7));
+        wait.Until(ExpectedConditions.UrlContains("https://staff-testing.testkontur.ru/news"));
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/communities/a75b6656-92dd-4f99-8e8c-7262e8477594");
+
+        var actions = driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/div/div/section/div[2]/div[2]/div[2]/div/span/button"));
+        actions.Click();
+
+        var checkboxEmail = driver.FindElement(By.CssSelector("[data-tid='Subscribe']"));
+        checkboxEmail.Click();
+    }
+
     public void Autorization()
     {
         var options = new ChromeOptions();
-        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        options.AddArguments("--no-sandbox", "--disable-extensions");
         
-        // зайти в chrome
-        var driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
         
-        // перейти по URL
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-        Thread.Sleep(5000);
         
-        // ввести логин и пароль
         var login = driver.FindElement(By.Id("Username"));
         login.SendKeys("korotaevamg@mail.ru");
 
         var password = driver.FindElement(By.Name("Password"));
         password.SendKeys(("@mzmVFsz3110"));
-        Thread.Sleep(3000);
         
-        // нажать кнопку "войти"
         var enter = driver.FindElement(By.Name("button"));
-        enter.Click();
-        Thread.Sleep(3000);
-        
-        // проверка того, что мы находимся на нужной странице
-        var currentUrl = driver.Url;
-        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
-            
-        // закрыть браузер
+        enter.Click(); 
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Close(); 
         driver.Quit();
     }
 }
--- testSeleniumGitExtension/obj/Debug/net8.0/testSeleniumGitExtension.AssemblyInfo.cs
+++ testSeleniumGitExtension/obj/Debug/net8.0/testSeleniumGitExtension.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testSeleniumGitExtension")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a725ab52ae532674b032a4a4f4a94a640089bf9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+426e04d530e8bd481488a4aeec928dd52dbe5eee")]
 [assembly: System.Reflection.AssemblyProductAttribute("testSeleniumGitExtension")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testSeleniumGitExtension")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
